355845b19003e0f0f00273e1702f1b9bbcc04313
Bruno <brusMX@users.noreply.github.com>
2016-10-24 19:02:15 -0700

    one more time

--- app.js
+++ app.js
@@ -21,14 +21,14 @@ var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-
+function getMessages(req, res, next) {
+    res.send('<html><body><iframe src\'https://webchat.botframework.com/embed/dxtraining?s=uh0tMFuMOE8.cwA.b-4.JbRhj_AJYwgG1HaNmtY5mW50yT3ryUzgi1kmirc_AHg\'></iframe></body></html>');
+});
+server.get('/', getMessages);
 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
-server.get('/', function (req, res, next) {
-    res.send('<html><body><iframe src\'https://webchat.botframework.com/embed/dxtraining?s=uh0tMFuMOE8.cwA.b-4.JbRhj_AJYwgG1HaNmtY5mW50yT3ryUzgi1kmirc_AHg\'></iframe></body></html>');
-    return next();
-});
+
 
 //=========================================================
 // Bots Dialogs
