e644dfddf72c24b74608fdc56bd9c2504b97f5bd
Bruno <brusMX@users.noreply.github.com>
2016-10-24 18:42:53 -0700

    would this work?

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 /*-----------------------------------------------------------------------------
 A simple "Hello World" bot for the Microsoft Bot Framework. 
 -----------------------------------------------------------------------------*/
@@ -20,8 +21,14 @@ var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
+
+
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
+server.get('/', function (req, res, next) {
+    res.send('<html><body><iframe src\'https://webchat.botframework.com/embed/dxtraining?s=uh0tMFuMOE8.cwA.b-4.JbRhj_AJYwgG1HaNmtY5mW50yT3ryUzgi1kmirc_AHg\'></iframe></body></html>');
+    return next();
+});
 
 //=========================================================
 // Bots Dialogs
